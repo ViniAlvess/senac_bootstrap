11fc74addddfaf80f8d808b5e49171801e253a58
Vinicios Alves Rocha <vinicios.arochasup@senacsp.edu.br>
2023-05-11 11:50:43 -0300

    home_menu

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, template_rendered,request 
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index(): 
-    return 'Olá Mundo'
+    return render_template ('index.html')
 
+@app.route('/base')
+def base(): 
+    return render_template('base.html')
+
+@app.route('/cursos')
+def cursos(): 
+    return render_template('cursos.html')
+
+@app.route('/login')
+def login(): 
+    return render_template('login.html')
+
+@app.route('/ead')
+def ead(): 
+    return render_template('ead.html')
+
+@app.route('/contato')
+def contato(): 
+    return render_template('contato.html')
 if __name__ =='__main__':
     app.run(debug=True)
