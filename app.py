1a208c159ce3d59bdf8eb197e9eb42c95d11794a
Vinicios Alves Rocha <vinicios.arochasup@senacsp.edu.br>
2023-05-25 12:09:13 -0300

    meu

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template,request
+from forms import formLogin , formNovoUsuario
 
 app = Flask(__name__)
+
+app.config ['SECRET_KEY'] = '65ad3ecb328fe2f1b810b1031f31d119'
 
 @app.route('/')
 def index(): 
@@ -14,9 +17,15 @@ def base():
 def cursos(): 
     return render_template('cursos.html')
 
-@app.route('/login')
+@app.route('/login', methods=['get','post'])
 def login(): 
-    return render_template('login.html')
+    titulo ='Login de acesso'
+    descricao = 'Formulario de login'
+    
+    form_login = formLogin()
+    form_novo_usuario = formNovoUsuario()
+    
+    return render_template('login.html',titulo=titulo, descricao=descricao,form_login=form_login,form_novo_usuario=form_novo_usuario)
 
 @app.route('/ead')
 def ead(): 
@@ -25,5 +34,6 @@ def ead():
 @app.route('/contato')
 def contato(): 
     return render_template('contato.html')
+
 if __name__ =='__main__':
     app.run(debug=True)
