ac5c303f5b5550a4686b3ff4a5f059940ccc8317
Vinicios Alves Rocha <vinicios.arochasup@senacsp.edu.br>
2023-06-01 12:02:35 -0300

    meu

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template,request
+import mysql.connector  
+from flask import Flask, render_template,request, url_for, flash, redirect 
 from forms import formLogin , formNovoUsuario
+from hashlib import sha256
 
 app = Flask(__name__)
 
 app.config ['SECRET_KEY'] = '65ad3ecb328fe2f1b810b1031f31d119'
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'P@$$w0rd',
+    database = 'ead_senac',
+)
 
 @app.route('/')
 def index(): 
@@ -25,7 +34,16 @@ def login():
     form_login = formLogin()
     form_novo_usuario = formNovoUsuario()
     
-    return render_template('login.html',titulo=titulo, descricao=descricao,form_login=form_login,form_novo_usuario=form_novo_usuario)
+    if form_login.validate_on_submit() and 'submitLogin' in request.form:
+        flash(f'Login realizado com sucesso: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('index'))
+    
+    if form_novo_usuario.validate_on_submit() and 'submitCadastro' in request.form:
+    
+     cursor = mydb.cursor()
+
+    
+    return render_template('login.html',titulo=titulo,descricao=descricao,form_login=form_login,form_novo_usuario=form_novo_usuario)
 
 @app.route('/ead')
 def ead(): 
